3764c552eec8a9cf92099c79de69c5071b8f843c
Guilherme Ávila <90806129+guilhermeavila77@users.noreply.github.com>
2023-04-15 22:19:43 -0300

    Upload aula 11
    Aula sobre Math

---
+++ Basico sobre JS/Aula 11 - Math/index.js
@@ -0,0 +1,19 @@
+let num1 = 9.49;
+let num2 = Math.floor(num1); //Arredonda para baixo
+console.log(num2);
+
+let num3 = Math.ceil(num1); //Arredonda para cima
+console.log(num3);
+
+let num4 = Math.round(num1); //Arredonda da maneira padrão
+console.log(num4);
+
+
+//Procura o maior numero de uma sequencia
+console.log(Math.max(1,2,3,5,96,4,5));
+
+//Procura o menor numero de uma sequencia
+console.log(Math.min(1,2,3,5,96,4,5));
+
+//Gera numero aleatorio
+console.log(Math.random() * (10 - 5) + 5);
