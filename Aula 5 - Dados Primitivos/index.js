3e533f61bf45740fdc3d5a4c62c50e7c2be6e7a6
Guilherme Ávila <90806129+guilhermeavila77@users.noreply.github.com>
2023-04-15 22:02:23 -0300

    Delete Aula 5 - Dados Primitivos directory

--- Aula 5 - Dados Primitivos/index.js
+++
@@ -1,7 +0,0 @@
-//Tipos de dados
-const nome = 'Guilherme' //String
-const num = 10 //Number
-const nomeNum = (nome, mun) //String
-let aluno; //undefined -> não aponta local na memoria 
-const sobreNome = null; //nulo -> não aponta lugar na memoria
-const boolean = true; //Valor lógico, true or false
