4bf0cc309a798b5be2c86144953a79726d191fd8
Guilherme Ávila <90806129+guilhermeavila77@users.noreply.github.com>
2023-04-18 23:52:47 -0300

    Upload aula 12
    Aula sobre arrays

---
+++ Basico sobre JS/Aula 12 - Array/index.js
@@ -0,0 +1,28 @@
+const alunos = ['Luiz', 'Maria', 'João'];
+console.log(alunos);
+
+console.log(alunos[1]);
+
+//Saber tamanho do array
+console.log(alunos.length);
+
+//Editar volor na array
+alunos[0] = 'Novo aluno';
+
+//Criando novo elemento
+aluno[3] = 'Luiza';
+
+//Coloca na ultima posição
+alunos.push('Guilherme');
+
+//Adicionar no começo, index 0
+alunos.unshift('Valentina');
+
+//Remover no final
+alunos.pop(); //Se colocar numa variavel ela vai guardar o valor removido
+
+//Remove do começo 
+alunos.shift();
+
+//Apagar valor (manter indices)
+delete alunos(1);
