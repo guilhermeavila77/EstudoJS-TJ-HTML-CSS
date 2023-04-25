9803c958e398c9edec4bc9d43e9dbe17d2396014
guilhermeavila77 <guialmeidaavila@gmail.com>
2023-04-24 23:42:19 -0300

    Correção do exercicio

--- Basico sobre JS/Aula 16 - Exercicios de conclusão/js/script.js
+++ Basico sobre JS/Aula 16 - Exercicios de conclusão/js/script.js
@@ -4,24 +4,27 @@ function funcaoPrincipal(){
 
     const pessoas = [];
 
-    function receberEventoForm (evento){
-        evento.preventDefalt(); //Não deixa que o navegador envie o formulario e apague tudo
+    function receberEventoForm (evento) {
+        evento.preventDefault();
         const nome =form.querySelector('.nome');
         const sobrenome =form.querySelector('.sobrenome');
         const peso =form.querySelector('.peso');
         const altura =form.querySelector('.altura');
         pessoas.push({
-            nome: nome.value,
-            sobrenome: sobrenome.value,
-            peso: peso.value,
-            altura: altura.value
+            nome: nome,
+            sobrenome: sobrenome,
+            peso: peso,
+            altura: altura
         });
         console.log(pessoas);
-        resultado.innerHTML += <p>`${nome}`</p>
-        resultado.innerHTML += <p>`${sobrenome}`</p>
-        resultado.innerHTML += <p>`${peso}`</p>
-        resultado.innerHTML += <p>`${altura}`</p>
+        resultado.innerHTML = ``;
+        resultado.innerHTML += `<p>Seu nome é ${nome.value}</p>`;
+        resultado.innerHTML += `<p>Seu sobrenome é ${sobrenome.value}<p>`;
+        resultado.innerHTML += `<p>Seu peso é ${peso.value}<p>`;
+        resultado.innerHTML += `<p>Sua altura é ${altura.value}<p>`;
     };
+
     form.addEventListener('submit', receberEventoForm);
+
 }
 funcaoPrincipal();
